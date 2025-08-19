caf0246fcc07b68dc7f38408e7e95a75fc426e53
Harry Stevens <harry.stevens@nytimes.com>
2025-08-19 13:02:25 -0400

    Fix prepublish and preversion issues

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,44 @@
+// eslint.config.js
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from 'eslint-plugin-storybook';
 
-import prettier from 'eslint-config-prettier';
+import prettierConfig from 'eslint-config-prettier';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import { fileURLToPath } from 'node:url';
 import svelteConfig from './svelte.config.js';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-	includeIgnoreFile(gitignorePath),
-	js.configs.recommended,
-	...svelte.configs.recommended,
-	prettier,
-	...svelte.configs.prettier,
-	{
-		languageOptions: {
-			globals: { ...globals.browser, ...globals.node }
-		}
-	},
-	{
-		files: ['**/*.svelte', '**/*.svelte.js'],
-		languageOptions: { parserOptions: { svelteConfig } }
-	},
-	...storybook.configs['flat/recommended']
+  includeIgnoreFile(gitignorePath),
+
+  js.configs.recommended,
+  ...svelte.configs.recommended,
+  ...storybook.configs['flat/recommended'],
+
+  // Turn off stylistic rules that conflict with Prettier (for JS + Svelte)
+  ...svelte.configs.prettier,
+  prettierConfig,
+
+  {
+    files: ['**/*.{js,ts,svelte}'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node },
+    },
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      'prettier/prettier': 'error'
+    }
+  },
+  {
+    files: ['**/*.svelte', '**/*.svelte.js'],
+    languageOptions: { parserOptions: { svelteConfig } }
+  }
 ];
